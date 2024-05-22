afc2752db32118aa694409c9a7e942a46f5a820a
dvnam1605 <nam.dv225050@sis.hust.edu.vn>
2024-05-22 21:36:54 +0700

    them 3 nut 'them nguoi','tat phat hien','phat hien ten'

---
+++ openCV/build/intermediates/cxx/Debug/11w1j412/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\NAM\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\NAM\\FaceDetection_Opencv4android\\openCV\\.cxx\\Debug\\11w1j412\\x86" ^
+  opencv_jni_shared
