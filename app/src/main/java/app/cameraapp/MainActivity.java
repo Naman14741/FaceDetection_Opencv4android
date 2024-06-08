4454079880b22c2185f7092e0e84a93d3c8169b7
Nguyễn Văn Nam <131948589+Naman14741@users.noreply.github.com>
2024-06-08 10:23:16 +0700

    update layout

--- app/src/main/java/app/cameraapp/MainActivity.java
+++ app/src/main/java/app/cameraapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -99,6 +100,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         db = new Database(this);
